db417b2cdcfd6186468beed3128a68a6c4df43e1
Abdul Raof-PC <abdulraof628@gmail.com>
2020-08-18 21:10:05 +0800

    Add validation

--- lib/pages/add_food.dart
+++ lib/pages/add_food.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:food_order/pages/home.dart';
 
 class AddFood extends StatefulWidget{
   @override
@@ -8,56 +7,124 @@ class AddFood extends StatefulWidget{
 }
 
 class _AddFoodState extends State<AddFood> {
+  
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Add New Item'),
-        ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: new TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Name'
-              ),
-            )
+      return Scaffold(
+        appBar: 
+          AppBar(
+            title: Text('Add New Item'),
           ),
-          Padding(
-            padding: EdgeInsets.only(left: 16.0, right: 16.0),
-            child: new TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Price'
-              ),
-            )
-          ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 16.0, right: 16.0),
-                  child: FlatButton(
-                    color: Colors.blue, 
-                    child: Text('Save'),
-                    onPressed: (){
-                    },
-                  ),
+        body: 
+          ListView(
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 10),
+                      child: TextFormField(
+                        autofocus: true,
+                        decoration: InputDecoration(
+                          labelText: 'Name'
+                        ),
+                        validator: (val){
+                          if(val.isEmpty){
+                            return 'Please enter name';
+                          }
+                          else{
+                            return null;
+                          }
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 10),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Price'
+                        ),
+                        validator: (val){
+                          if(val.isEmpty){
+                            return 'Please enter price';
+                          }
+                          else{
+                            return null;
+                          }
+                        },
+                      ),
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: EdgeInsets.only(top: 16.0),
-                  child: FlatButton(
-                    color: Colors.grey[200], 
-                    child: Text('Cancel'),
-                    onPressed: (){
-                      Navigator.pop(context);
-                    },
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 20, right: 20),
+                      child: RaisedButton(
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        child: Text('Save'),
+                        onPressed: (){
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: RaisedButton(
+                        color: Colors.grey[200],
+                        textColor: Colors.black,
+                        child: Text('Cancel'),
+                        onPressed: (){
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            )
-        ],
-      ),
-    );
+              ]
+          )
+        // body: Column(
+        //   children: [
+        //     Padding(
+        //       padding: EdgeInsets.all(16.0),
+        //       child: new 
+        //     ),
+        //     Padding(
+        //       padding: EdgeInsets.only(left: 16.0, right: 16.0),
+        //       child: new TextFormField(
+        //         decoration: InputDecoration(
+        //           labelText: 'Price'
+        //         ),
+        //       )
+        //     ),
+        //       Row(
+        //         mainAxisAlignment: MainAxisAlignment.center,
+        //         children: [
+        //           Padding(
+        //             padding: EdgeInsets.only(top: 16.0, right: 16.0),
+        //             child: FlatButton(
+        //               color: Colors.blue, 
+        //               child: Text('Save'),
+        //               onPressed: (){
+        //               },
+        //             ),
+        //           ),
+        //           Padding(
+        //             padding: EdgeInsets.only(top: 16.0),
+        //             child: FlatButton(
+        //               color: Colors.grey[200], 
+        //               child: Text('Cancel'),
+        //               onPressed: (){
+        //                 Navigator.pop(context);
+        //               },
+        //             ),
+        //           ),
+        //         ],
+        //       )
+        //   ],
+        // ),
+      );
   }
 }
